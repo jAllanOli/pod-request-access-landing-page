98b211c53ffe18208c9013691aba646c733eab1c
Allan <j.a.o.huthan@gmail.com>
2023-08-07 14:22:07 -0300

    form added and jest setup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,60 @@
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { render, screen } from '@testing-library/angular';
+import { fireEvent, render, screen } from '@testing-library/angular';
+import userEvent from '@testing-library/user-event';
 
 const sut = async () => {
-  await render(AppComponent);
+  await render(AppComponent, {
+    imports: [FormsModule],
+  });
 };
 
 describe('AppComponent', () => {
+  let input: HTMLInputElement;
+
   beforeEach(async () => {
     await sut();
+    input = screen.getByTestId('email-input');
   });
 
-  it('should render the title', async () => {
+  it('should render the title', () => {
     expect(screen.getByText('Publish your podcasts')).toBeTruthy();
   });
 
-  it('should remder the body text', () => {
+  it('should render the body text', () => {
     expect(
       screen.getByText(
         'Upload your audio to Pod with a single click. We’ll then distribute your podcast to Spotify, Apple Podcasts, Google Podcasts, Pocket Casts and more!'
       )
     ).toBeTruthy();
+  });
+
+  it('should render the email input', () => {
+    expect(input).toBeTruthy();
+  });
+
+  it('should render the submit button', () => {
+    expect(input).toBeTruthy();
+  });
+
+  it('should not display the error message when the aplication starts', () => {
+    expect(screen.queryByTestId('error-message')).toBeFalsy();
+  });
+
+  it('should display the error message when user interacts with the input and it is still empty', async () => {
+    fireEvent.click(input);
+    fireEvent.blur(input);
+    expect(screen.getByTestId('error-message')).toBeTruthy();
+  });
+
+  it('should display the error message when the email provided is not valid', () => {
+    userEvent.type(input, 'teste');
+    fireEvent.blur(input);
+    expect(screen.getByTestId('error-message')).toBeTruthy();
+  });
+
+  it('should not display the error message when the email provided is valid', () => {
+    userEvent.type(input, 'email@email.com');
+    expect(screen.queryByTestId('error-message')).toBeFalsy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  mail: string = '';
+  onSubmit(data: NgForm): void {
+    console.log(data.value.email);
+  }
 }
